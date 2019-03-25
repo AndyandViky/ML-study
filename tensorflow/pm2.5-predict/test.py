dc54bdb605595ce48b91a6f6471139e3fc995216
andy <andy_viky@163.com>
2019-03-25 10:37:02 +0800

    test linear model

--- tensorflow/pm2.5-predict/test.py
+++ tensorflow/pm2.5-predict/test.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
 import numpy as np
 
-init = tf.global_variables_initializer()
-sess = tf.Session()
+x = tf.constant([[1], [2], [3], [4]], dtype=tf.float32)
+y_true = tf.constant([[0], [-1], [-2], [-3]], dtype=tf.float32)
 
-sess.run(init)
+linear_model = tf.layers.Dense(units=1)
+y_pred = linear_model(x)
+loss = tf.losses.mean_squared_error(labels=y_true, predictions=y_pred)
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
 
-for step in range(100):
-    sess.run()
-
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(1000):
+        _, loss_value = sess.run((train, loss))
+        print(loss_value)
+    print(sess.run(y_pred))
