f62270230417f6d320c707115bb365038ca202a5
andy <andy_viky@163.com>
2019-03-25 21:19:48 +0800

    use keras test

--- tensorflow/pm2.5-predict/test.py
+++ tensorflow/pm2.5-predict/test.py
@@ -1,18 +1,43 @@
-import tensorflow as tf
 import numpy as np
+import gzip
+import struct
+import keras as ks
+import logging
+from keras.layers import Dense, Activation, Flatten, Convolution2D
+from keras.utils import np_utils
 
-x = tf.constant([[1], [2], [3], [4]], dtype=tf.float32)
-y_true = tf.constant([[0], [-1], [-2], [-3]], dtype=tf.float32)
 
-linear_model = tf.layers.Dense(units=1)
-y_pred = linear_model(x)
-loss = tf.losses.mean_squared_error(labels=y_true, predictions=y_pred)
-optimizer = tf.train.GradientDescentOptimizer(0.01)
-train = optimizer.minimize(loss)
+def read_data(label_url,image_url):
+    with gzip.open(label_url) as flbl:
+        magic, num = struct.unpack(">II",flbl.read(8))
+        label = np.fromstring(flbl.read(),dtype=np.int8)
+    with gzip.open(image_url,'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII",fimg.read(16))
+        image = np.fromstring(fimg.read(),dtype=np.uint8).reshape(len(label),rows,cols)
+    return (label, image)
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for step in range(1000):
-        _, loss_value = sess.run((train, loss))
-        print(loss_value)
-    print(sess.run(y_pred))
+
+(train_lbl, train_img) = read_data('../MNIST_data/train-labels-idx1-ubyte.gz','../MNIST_data/train-images-idx3-ubyte.gz')
+(val_lbl, val_img) = read_data('../MNIST_data/t10k-labels-idx1-ubyte.gz','../MNIST_data/t10k-images-idx3-ubyte.gz')
+
+
+def to4d(img):
+    return img.reshape(img.shape[0],784).astype(np.float32)/255
+
+
+train_img = to4d(train_img)
+val_img = to4d(val_img)
+#important
+train_LBL = np_utils.to_categorical(train_lbl)
+val_LBL = np_utils.to_categorical(val_lbl)
+
+model = ks.models.Sequential()
+model.add(Dense(128,input_dim=784))
+model.add(Activation('relu'))
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
+model.fit(x=train_img,y=train_LBL,batch_size=100,nb_epoch=10,verbose=1,validation_data=(val_img,val_LBL))
