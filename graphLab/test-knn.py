f89317e51cf63ffbbd4069355d343bc7e3c9997b
andy <andy_viky@163.com>
2019-05-02 19:29:11 +0800

    add knn recommond module

--- graphLab/test-knn.py
+++ graphLab/test-knn.py
@@ -2,13 +2,12 @@
 
 import graphlab
 
+knn_model = graphlab.load_model("knn_model")
+
 datas = graphlab.SFrame('./datasets/people_wiki.gl')
 datas['word_count'] = graphlab.text_analytics.count_words(datas['text'])
 tfidf = graphlab.text_analytics.tf_idf(datas['word_count'])
 datas['tfidf'] = tfidf # 将计算出来的tfidf赋给语料库
-knn_model = graphlab.nearest_neighbors.create(datas, features=['tfidf'], label='name')
 
-knn_model.save('knn_model')
-
-# result = knn_model.query(datas[datas['name'] == 'Barack Obama'], k=10)
-# print(result)
+result = knn_model.query(datas[datas['name'] == 'Barack Obama'], k=10)
+print(result)
