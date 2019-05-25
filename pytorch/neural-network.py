2af8d13b3078680c7bb6ccc8dbdaa9a635d1e88e
andy <andy_viky@163.com>
2019-05-25 10:34:48 +0800

    add simple classifier by torch

--- pytorch/neural-network.py
+++ pytorch/neural-network.py
@@ -6,5 +6,114 @@
 @csdn: https://blog.csdn.net/AndyViky
 @file: neural-network.py
 @time: 2019/5/24 下午7:13
-@desc:
+@desc: 使用pytorch创建一个简单的神经网络
 '''
+
+import torch
+import torch.nn as nn # 神经网络包
+import torch.nn.functional as F
+import torch.optim as optim # 激活函数包
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        new_features = 1
+        for s in size:
+            new_features *= s
+        return new_features
+
+if __name__ == '__main__':
+    # test
+    net = Net()
+    # print(net)
+
+    # params = list(net.parameters())
+    # print(len(params))
+    # print(params[0].size()) # conv1's .weight
+
+    input = torch.randn(1, 1, 32, 32) # 从正态分布中随机抽取
+    # print(input)
+    # out = net(input)
+    # print(out)
+
+    # net.zero_grad() # 使用随机梯度将所有参数和反向支持的梯度缓冲区归零
+    # out.backward(torch.randn(1, 10))
+
+    '''
+    torch.nn仅支持 mini-batch， 因此为输入只有一个 sample 时使用 input.unsqueeze(0) 添加假的 sample
+    '''
+
+    # -----------------Loss-----------------
+    # Loss
+    # output = net(input)
+    target = torch.randn(10)
+    target = target.view(1, -1)
+    criterion = nn.MSELoss()
+    # loss = criterion(output, target)
+    # print(loss)
+
+    # net.zero_grad()
+    # print('conv1.bias.grad before backward')
+    # print(net.conv1.bias.grad)
+    #
+    # loss.backward()
+    #
+    # print('conv1.bias.grad after backward')
+    # print(net.conv1.bias.grad)
+    # ----------------------------------
+
+
+    # Update the weights
+    # ----简单python代码实现weight更新----
+    # params = net.parameters()
+    # learning_rate = 0.01
+    # for f in params:
+    #     f.data.sub_(f.grad.data * learning_rate)
+    # ----------------------------------
+
+    # ----use optim 包----
+    optimizer = optim.SGD(net.parameters(), lr=0.01)
+    optimizer.zero_grad()
+    output = net(input)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step() # Does the update
+    # ----------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pytorch/nn-classifier.py
+++ pytorch/nn-classifier.py
@@ -6,5 +6,87 @@
 @csdn: https://blog.csdn.net/AndyViky
 @file: nn-classifier.py
 @time: 2019/5/25 上午8:21
-@desc:
+@desc: 使用pytorch创建一个简单的分类器
 '''
+
+'''
+1 Load and normalizing the CIFAR10 training and test datasets using torchvision
+2 Define a Convolutional Neural Network
+3 Define a loss function
+4 Train the network on the training data
+5 Test the network on the test data
+'''
+
+import torch
+import torch.utils as utils
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainset = torchvision.datasets.CIFAR10(root = '../datasets', train = True,
+                                        download = True, transform = transform)
+trainloader = utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='../datasets', train=False,
+                                       download=True, transform=transform)
+testloader = utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+# -----------------img-show-----------------
+# show image
+import matplotlib.pyplot as plt
+import numpy as np
+
+def img_show(img):
+    img = img / 2 + 0.5
+    nimg = img.numpy()
+    plt.imshow(np.transpose(nimg, (1, 2, 0)))
+    plt.imshow(nimg)
+    plt.show()
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+img_show(torchvision.utils.make_grid(images)) # make_grid的作用是将若干幅图像拼成一幅图像
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# --------------------------------------
+
+# -----------------定义网络结构-----------------
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
