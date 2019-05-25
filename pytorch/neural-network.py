23174b53c65b3776f0a4e98be54451ef6c4cea0a
andy <andy_viky@163.com>
2019-05-25 10:33:51 +0800

    create neural-network by torch

---
+++ pytorch/neural-network.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+'''
+@author: andy
+@contact: andy_viky@163.com
+@github: https://github.com/AndyandViky
+@csdn: https://blog.csdn.net/AndyViky
+@file: neural-network.py
+@time: 2019/5/24 下午7:13
+@desc:
+'''
---
+++ pytorch/nn-classifier.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+'''
+@author: andy
+@contact: andy_viky@163.com
+@github: https://github.com/AndyandViky
+@csdn: https://blog.csdn.net/AndyViky
+@file: nn-classifier.py
+@time: 2019/5/25 上午8:21
+@desc:
+'''
